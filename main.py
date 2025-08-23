8309faa87daa92ba6b62da2cd21deb641a6b208e
Elijah Johnson <97414090+ej-east@users.noreply.github.com>
2025-08-23 18:47:20 -0400

    Added the ability to download YouTube video & use local video files

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from dotenv import load_dotenv
 from modules.AI.main import AI
+from modules.videos.main import VideoProccesser
 import os
 
 load_dotenv()
+
+URL = r"https://www.youtube.com/watch?v=OIenNRt2bjg"
 
 def main() -> None:
     api_key = os.getenv("GEMINI_API_KEY")
     if not api_key:
         return
     
+    videos = VideoProccesser()
     ai = AI(api_key=api_key)
-    transcription = ai.transcribe_audio("voice.wav")
+    
+    video_path = videos.download_video(url=URL)
+    if not video_path:
+        return
+    
+    audio_path = videos.extract_audio(video_path)
+    
+    if not audio_path:
+        return
+    
+    transcription = ai.transcribe_audio(audio_path)
     if not transcription:
         return
     
---
+++ modules/videos/main.py
@@ -0,0 +1,73 @@
+import logging, yt_dlp, ffmpeg
+from pathlib import Path
+from os.path import getctime
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+class VideoProccesser:
+    def __init__(self, video_output_dir : str = "downloads", audio_output_dir : str = "audio") -> None:
+        logger.info("Successfully loaded Video Proccesser module")
+        
+        self.video_output_dir = video_output_dir
+        self.audio_output_dir = audio_output_dir
+        
+        self.youtube_options = {
+            'outtmpl' : f'{self.video_output_dir}/%(totle)s.%(ext)s',
+            'format': 'best[height<=729]/best'
+        }
+        
+        Path(self.video_output_dir).mkdir(exist_ok=True)
+        Path(self.audio_output_dir).mkdir(exist_ok=True)
+
+    def download_video(self, url : str) -> str | None:
+        logger.info("Starting YouTube video download")
+        with yt_dlp.YoutubeDL(self.youtube_options) as youtube_downloader:
+            info = youtube_downloader.extract_info(url, download=False)
+            
+            if not info:
+                logger.warning("No information on YouTube video found")
+                return
+            
+        video_title = info.get('title', 'Unknown')
+        duration = info.get('duration', 0)
+        
+        youtube_downloader.download([url])
+        
+        # Now we need to figure which file we just downloaded....
+        
+        ## First approach is to list all of the files and see if it matches what we expect the filename to be
+        for file in tuple(Path(self.video_output_dir).iterdir()):
+            if file and file.is_file() and video_title.replace('/', '_') in file.stem:
+                logger.info(f"Downloaded YouTube Video: {file}")
+                return str(file)
+        
+        ## Second approach is to get the file that is the youngest
+        files = list(Path(self.video_output_dir).iterdir())
+        if files:
+            latest_file = max(files, key=getctime)
+            logger.info(f"Downloaded YouTube Video: {latest_file}")
+            return str(file)
+        return
+    
+    def extract_audio(self, filepath : str) -> str | None:
+        audio_format = ".wav"
+        video_path = Path(filepath)
+        audio_output_dir = Path(self.audio_output_dir)
+        audio_output_path = audio_output_dir / f"{video_path.stem}{audio_format}"
+
+        if not video_path or not video_path.exists():
+            logger.warning("Video file was not found")
+            return
+        
+        logger.info("Extracting audio")
+        ffmpeg.input(str(video_path)).output(str(audio_output_path), acodec='pcm_s16le', ac=1, ar='16000').overwrite_output().run(quiet=True, capture_stderr=True)
+        
+        logger.info(f"Extracted Audio - Filename: {str(audio_output_path)}")
+        return str(audio_output_path)
+        
+        
