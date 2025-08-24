a865df4b76c68c6cdce843e6c2e7a76c5f5758cc
Elijah Johnson <97414090+ej-east@users.noreply.github.com>
2025-08-24 12:07:28 -0400

    Updated main file  to remove uneeded files

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ from dotenv import load_dotenv
 from modules.AI.main import AI
 from modules.videos.main import VideoProccesser
 from modules.notion.main import NotionDB
-import os
+import os, logging
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 URL = r"https://www.youtube.com/watch?v=DIeP3Y5VdV8&pp=ygUHYXdzIHNhYQ%3D%3D"
 
 
 def main() -> None:
+    using_notion = True
     gemini_api_key = os.getenv("GEMINI_API_KEY")
     
     notion_api_key = os.getenv("NOTION_API_KEY")
@@ -19,12 +28,14 @@ def main() -> None:
         return
     
     if not notion_api_key or not notion_database_id:
-        return
+        using_notion = False
     
     
     videos = VideoProccesser()
     ai = AI(api_key=gemini_api_key)
-    notion = NotionDB(api_key=notion_api_key, database_id=notion_database_id)
+    
+    if notion_api_key and notion_database_id and using_notion:
+        notion = NotionDB(api_key=notion_api_key, database_id=notion_database_id)
         
     video_path = videos.download_video(url=URL)
     if not video_path:
@@ -44,12 +55,24 @@ def main() -> None:
     if not llm_output:
         return
     
-    notion.add_entry(
-        title=llm_output["title"],
-        summary=llm_output["summary"],
-        key_points=llm_output["key_points"],
-        action_items=llm_output["action_items"]
-    )
+    if notion_api_key and notion_database_id and using_notion:
+        notion.add_entry(
+            title=llm_output["title"],
+            summary=llm_output["summary"],
+            key_points=llm_output["key_points"],
+            action_items=llm_output["action_items"]
+        )
+    else:
+        print(llm_output["title"])
+        print(llm_output["summary"])
+        print(llm_output["key_points"])
+        print(llm_output["action_items"])
+    
+    logger.info("Cleaning up uneeded files...")
+    os.remove(video_path)
+    os.remove(audio_path)
+    logger.info("Successfully ran program...")
+        
     
 
 if __name__ == "__main__":
