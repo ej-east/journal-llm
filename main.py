3c2e92ee9bb756a9d228173c5d24a745e952525d
Elijah Johnson <97414090+ej-east@users.noreply.github.com>
2025-08-23 20:40:07 -0400

    formatted output

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ load_dotenv()
 
 URL = r"https://www.youtube.com/watch?v=OIenNRt2bjg"
 
+def formatted_print(llm_output : dict) -> bool:
+    try:
+        title = llm_output["title"]
+        summary = llm_output["summary"]
+        key_points = llm_output["key_points"]
+        action_items = llm_output["action_items"]
+        print(f"# {title}")
+        print(f"## Summary\n{summary}")
+        
+        print()
+        
+        print(f"## Key Points")
+        for point in key_points:
+            print(f"- {point}")
+        
+        print()
+                
+        print(f"## Key Points")
+        
+        for action in action_items:
+            print(f"- {action}")
+        
+        
+    except Exception as e:
+        print("[-] Failed to print final output")
+        return False    
+    return True
+
+
+
 def main() -> None:
     api_key = os.getenv("GEMINI_API_KEY")
     if not api_key:
@@ -30,7 +60,10 @@ def main() -> None:
     
     summary = ai.get_llm_summary(transcription)
     
-    print(summary)
+    if not summary:
+        return
+    
+    formatted_print(summary)
 
 
 if __name__ == "__main__":
